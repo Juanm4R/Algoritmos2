f9144196e4001dbb9efa4709380ccfe36b137d5c
Juanm4R <110467078+Juanm4R@users.noreply.github.com>
2026-03-30 16:00:15 -0300

    interfaz diccionario multiple

--- src/bloque4/DiccionarioDeSinonimos.java
+++ src/bloque4/DiccionarioDeSinonimos.java
@@ -1,4 +1,44 @@
 package bloque4;
 
-public class DiccionarioDeSinonimos {
+import bloque4.Interfaz.DiccionarioMultiple;
+
+public class DiccionarioDeSinonimos implements DiccionarioMultiple {
+    private static final int MAX_CLAVES = 100;
+    private static final int MAX_VALOR = 100;
+
+    private int[] claves;
+    private int[][] valores;
+    private int[] cantValores;
+    private int cantClaves;
+
+
+    @Override
+    public void InicializarDiccionario() {
+        
+    }
+
+    @Override
+    public void Agregar(int clave, int valor) {
+
+    }
+
+    @Override
+    public void EliminarValor(int clave, int valor) {
+
+    }
+
+    @Override
+    public void Eliminar(int clave) {
+
+    }
+
+    @Override
+    public int[] Recuperar(int clave) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] Claves() {
+        return new int[0];
+    }
 }
