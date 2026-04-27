b2a3ade7af00d839650bbcdc975a97283e97f404
bautipol <bautipole2007@gmail.com>
2026-04-27 15:03:14 -0300

    bloque1

--- src/desafios/desafio4.java
+++ src/desafios/desafio4.java
@@ -1,6 +1,54 @@
 package desafios;
 
+import Bloque3.Implementacion.Estrategia_Conjuntos;
+import Bloque3.Interface.ConjuntosTDA;
+
 public class desafio4 {
+    public ConjuntosTDA diferenciaSimetrica(ConjuntosTDA a, ConjuntosTDA b) {
+        ConjuntosTDA resultado = new Estrategia_Conjuntos();
+        resultado.InicializarConjunto();
 
+        ConjuntosTDA copiaA = clonar(a);
+        ConjuntosTDA copiaB = clonar(b);
 
+        while (!copiaA.ConjuntoVacio()) {
+            int elemento = copiaA.Elegir();
+            if (!b.Pertenece(elemento)) {
+                resultado.Agregar(elemento);
+            }
+            copiaA.Sacar(elemento);
+        }
+
+        while (!copiaB.ConjuntoVacio()) {
+            int elemento = copiaB.Elegir();
+            if (!a.Pertenece(elemento)) {
+                resultado.Agregar(elemento);
+            }
+            copiaB.Sacar(elemento);
+        }
+
+        return resultado;
+    }
+
+    private ConjuntosTDA clonar(ConjuntosTDA origen) {
+        ConjuntosTDA destino = new Estrategia_Conjuntos();
+        ConjuntosTDA aux = new Estrategia_Conjuntos();
+        destino.InicializarConjunto();
+        aux.InicializarConjunto();
+
+        while (!origen.ConjuntoVacio()) {
+            int x = origen.Elegir();
+            aux.Agregar(x);
+            origen.Sacar(x);
+        }
+
+        while (!aux.ConjuntoVacio()) {
+            int x = aux.Elegir();
+            origen.Agregar(x);
+            destino.Agregar(x);
+            aux.Sacar(x);
+        }
+
+        return destino;
+    }
 }
