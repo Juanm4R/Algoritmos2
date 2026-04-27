a5c8f32d52dca08af32edf02fe305e2f724a8f60
jbonicalzi <jbonicalzi@uade.edu.ar>
2026-04-27 16:26:07 -0300

    Cargo los cambios y resolucion de Desafio 2

---
+++ src/desafios/Desafio2.java
@@ -0,0 +1,10 @@
+package desafios;
+
+import ImplementacionesDinamicas.colaConPrioridad.Implementacion.ColaPrioridadDinamica;
+
+//Desafío 2 — "El turno justo" (Cola con Prioridad dinámica) ¿Podés contar los elementos de P=1 sin "saltar" P=2? No: Desacolar() siempre extrae la mayor prioridad disponible. Para llegar a P=1 hay que vaciar P=2 primero, y restaurar tdo al terminar. El código termina siendo 15 líneas pero la lógica de restauración requiere pensar en 2 auxiliares.
+public class boolean Desafio2 (ColaPrioridadDinamica cola){
+    ColaPrioridadDinamica aux1 = new ColaPrioridadDinamica();
+    ColaPrioridadDinamica aux2 = new ColaPrioridadDinamica();
+
+}
