8fef7ff0de7e0f352fe604ce1e6f963306679207
bautipol <bautipole2007@gmail.com>
2026-04-27 15:14:18 -0300

    bloque1

--- src/desafios/Desafio1.java
+++ src/desafios/Desafio1.java
@@ -1,26 +1,44 @@
 package desafios;
 
+import ImplementacionesDinamicas.bloque1.Implementacion.PilaDinamica;
 import ImplementacionesDinamicas.bloque1.Interface.PilaTDA;
+import ImplementacionesEstaticas.bloque2.Implementacion.Estrategia_2_Cola;
 import ImplementacionesEstaticas.bloque2.Interface.ColaTDA;
 
-public static boolean Desafio1 (ColaTDA origen) {
-    ColaTDA copiaIdentica = new ColaTDA(); copiaIdentica.InicializarCola(); //para comparar
-    PilaTDA copiaInvertida = new PilaTDA(); copiaInvertida.InicializarPila();
-    ColaTDA aux = new ColaTDA(); aux.InicializarCola();
-    while (!origen.ColaVacia()){
-        int x = origen.Primero();
-        copiaIdentica.Acolar(x); copiaInvertida.Apilar(x); aux.Acolar(x);
-        origen.Desacolar();
-    }
-    while(!aux.ColaVacia()){
-        origen.Acolar(aux.Primero()); aux.Desacolar();
-    }
-    boolean coinciden = true; //Comparo Cola con Pila
-    while(!copiaIdentica.ColaVacia() && coinciden){
-        if(copiaIdentica.Primero() != copiaInvertida.Tope()){
-            coinciden = false;
+public class Desafio1 {
+
+    public static boolean desafio1(ColaTDA origen) {
+        ColaTDA copiaIdentica = new Estrategia_2_Cola();
+        PilaTDA copiaInvertida = new PilaDinamica();
+        ColaTDA aux = new Estrategia_2_Cola();
+
+        copiaIdentica.InicializarCola();
+        copiaInvertida.InicializarPila();
+        aux.InicializarCola();
+
+        while (!origen.ColaVacia()) {
+            int x = origen.Primero();
+            copiaIdentica.Acolar(x);
+            copiaInvertida.Apilar(x);
+            aux.Acolar(x);
+            origen.Desacolar();
         }
-        copiaIdentica.Desacolar(); copiaInvertida.Desapilar();
+
+        while (!aux.ColaVacia()) {
+            origen.Acolar(aux.Primero());
+            aux.Desacolar();
+        }
+
+        boolean coinciden = true;
+
+        while (!copiaIdentica.ColaVacia() && coinciden) {
+            if (copiaIdentica.Primero() != copiaInvertida.Tope()) {
+                coinciden = false;
+            }
+            copiaIdentica.Desacolar();
+            copiaInvertida.Desapilar();
+        }
+
+        return coinciden;
     }
-    return coinciden;
 }
